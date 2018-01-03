9e4ce5f2f0407fb12d47c7e01b002389a1e1167b
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2018-01-02 17:56:21 -0800

    instnace NVME support and VM performance tuning (#10)
    segment size = multiple of blocksize
    
    use XFS instead of EXT4, as it yields 10-15% more
    write bandwidth
    
    optimize XFS settings for RAID chunk size
    
    add option to optimize VM parameters
    
    support NVME RAID

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 
 setup(
     name="s3mi",
-    version="0.4.1",
+    version="0.4.2",
     url='https://github.com/chanzuckerberg/s3mi',
     license=open("LICENSE").readline().strip(),
     author='S3MI contributors',
